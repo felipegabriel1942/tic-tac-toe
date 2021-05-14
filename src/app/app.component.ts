6fdb89078ec12e051bdf6c9585e4ccf7d1ebb858
Felipe Gabriel <pinheiro_felipeg@yahoo.com.br>
2021-05-13 21:16:37 -0300

    Creating board

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'tic-tac-toe';
+export class AppComponent implements OnInit {
+  fieldValues: string[] = [];
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.createFields();
+  }
+
+  createFields(): void {
+    for (let i = 0; i < 9; i++) {
+      this.fieldValues.push(...[null]);
+    }
+  }
 }
