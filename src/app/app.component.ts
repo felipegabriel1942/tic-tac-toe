ddeae0484ea8701d8d17a0c67a6bd0a6bad79d4f
Felipe Gabriel <pinheiro_felipeg@yahoo.com.br>
2021-06-03 10:49:17 -0300

    Melhorando algoritmo de decisão do roboto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,6 +12,16 @@ export class AppComponent implements OnInit, AfterViewInit {
   playerOnTurn: Player;
   winnerPlayer: Player;
   players: Player[] = [];
+  victoryConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
 
   constructor(private modalService: ModalService) {}
 
@@ -23,21 +33,6 @@ export class AppComponent implements OnInit, AfterViewInit {
 
   ngAfterViewInit(): void {
     this.modalService.openModal('welcome');
-  }
-
-  restartGame(): void {
-    this.closeGameOverModal();
-    this.winnerPlayer = null;
-    this.createFields();
-    this.setFirstPlayer();
-  }
-
-  createFields(): void {
-    this.fieldValues = [];
-
-    for (let i = 0; i < 9; i++) {
-      this.fieldValues.push(null);
-    }
   }
 
   createPlayers(): void {
@@ -52,6 +47,25 @@ export class AppComponent implements OnInit, AfterViewInit {
     });
 
     this.players.push(...[player1, player2]);
+  }
+
+  restartGame(): void {
+    this.closeGameOverModal();
+    this.winnerPlayer = null;
+    this.createFields();
+    this.setFirstPlayer();
+  }
+
+  closeGameOverModal(): void {
+    this.modalService.closeModal('game-over');
+  }
+
+  createFields(): void {
+    this.fieldValues = [];
+
+    for (let i = 0; i < 9; i++) {
+      this.fieldValues.push(null);
+    }
   }
 
   setFirstPlayer(): void {
@@ -93,24 +107,20 @@ export class AppComponent implements OnInit, AfterViewInit {
     }
 
     setTimeout(() => {
-      this.chooseOneFieldForRoboto();
+      this.choseOneFieldForRoboto();
       this.checkGameState();
       this.changePlayerTurn();
     }, 500);
   }
 
-  chooseOneFieldForRoboto(): void {
-    const chosenField = Math.floor(Math.random() * 9);
-
-    if (this.isFieldAlredyMarked(chosenField)) {
-      this.chooseOneFieldForRoboto();
-    } else {
-      this.fieldValues[chosenField] = 1;
-    }
+  checkGameState(): void {
+    this.victoryConditions.forEach((indexes) => this.checkForWinner(indexes));
   }
 
-  isFieldAlredyMarked(index: number): boolean {
-    return this.fieldValues[index] != null;
+  changePlayerTurn(): void {
+    this.playerOnTurn = this.players.filter(
+      (player) => player.name !== this.playerOnTurn.name
+    )[0];
   }
 
   isGameWon(): boolean {
@@ -124,25 +134,81 @@ export class AppComponent implements OnInit, AfterViewInit {
     );
   }
 
-  checkGameState(): void {
-    const victoryConditions = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
-    ];
+  choseOneFieldForRoboto(): void {
+    let chosenField;
 
-    victoryConditions.forEach((indexes) => this.checkForWinner(indexes));
+    if (this.onlyOneFieldMarked()) {
+      chosenField = this.choseRandomField();
+    } else {
+      chosenField = this.choseBestField();
+    }
+
+    this.markRobotoField(chosenField);
+  }
+
+  markRobotoField(field: number): void {
+    if (this.isFieldAlredyMarked(field)) {
+      this.choseOneFieldForRoboto();
+    } else {
+      this.fieldValues[field] = 1;
+    }
+  }
+
+  isFieldAlredyMarked(index: number): boolean {
+    return this.fieldValues[index] != null;
+  }
+
+  choseRandomField(): number {
+    return Math.floor(Math.random() * 9);
+  }
+
+  choseBestField(): number {
+    const linesWithTwoFieldMarked = this.getLinesWithTwoFieldsMarked();
+
+    const linesMarkedByRoboto = this.getLinesMarkedByRoboto(
+      linesWithTwoFieldMarked
+    );
+
+    if (linesMarkedByRoboto.length > 0) {
+      return this.getFieldNotMarkedInLine(linesMarkedByRoboto[0]);
+    } else if (
+      linesMarkedByRoboto.length === 0 &&
+      linesWithTwoFieldMarked.length > 0
+    ) {
+      return this.getFieldNotMarkedInLine(linesWithTwoFieldMarked[0]);
+    } else {
+      return this.choseRandomField();
+    }
+  }
+
+  getLinesWithTwoFieldsMarked(): number[][] {
+    return this.victoryConditions
+      .filter((line) => this.lineHaveOnlyOneFieldLeft(line))
+      .filter((line) => this.lineHaveTwoFieldsMarkedBySamePlayer(line));
+  }
+
+  getLinesMarkedByRoboto(lines: number[][]): number[][] {
+    return lines.filter((line) => line.reduce((acc, v) => acc + v) === 2);
+  }
+
+  getFieldNotMarkedInLine(line: number[]): number {
+    return line.filter((field) => this.fieldValues[field] == null)[0];
+  }
+
+  lineHaveOnlyOneFieldLeft(line: number[]): boolean {
+    return this.getMarkedFields(line).length === 2;
+  }
+
+  lineHaveTwoFieldsMarkedBySamePlayer(line: number[]): boolean {
+    return this.getMarkedFields(line).reduce((acc, v) => acc + v) !== 1;
+  }
+
+  onlyOneFieldMarked(): boolean {
+    return this.fieldValues.filter((v) => v != null).length === 1;
   }
 
   checkForWinner(indexes: number[]): void {
-    const markedFields = indexes
-      .map((index) => this.fieldValues[index])
-      .filter((value) => value != null);
+    const markedFields = this.getMarkedFields(indexes);
 
     if (markedFields.length !== indexes.length) {
       return;
@@ -159,22 +225,17 @@ export class AppComponent implements OnInit, AfterViewInit {
     }
   }
 
+  getMarkedFields(fields: number[]): number[] {
+    return fields
+      .map((index) => this.fieldValues[index])
+      .filter((value) => value != null);
+  }
+
   isVictorious(fieldsValuesSum: number): boolean {
     return fieldsValuesSum === 0 || fieldsValuesSum === 3;
   }
-
-  changePlayerTurn(): void {
-    this.playerOnTurn = this.players.filter(
-      (player) => player.name !== this.playerOnTurn.name
-    )[0];
-  }
-
   openGameOverModal(): void {
     this.modalService.openModal('game-over');
-  }
-
-  closeGameOverModal(): void {
-    this.modalService.closeModal('game-over');
   }
 
   closeWelcomeModal(): void {
